608c9569981b7d63ac5cc0cea1fb884579b312ce
ftery0 <lerb5253@dgsw.hs.kr>
2025-03-20 10:10:47 +0900

    init

---
+++ src/main/java/b1nd/b1nd_website_server/B1ndWebsiteServerApplication.java
@@ -0,0 +1,13 @@
+package b1nd.b1nd_website_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B1ndWebsiteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B1ndWebsiteServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b1nd/b1nd_website_server/B1ndWebsiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package b1nd.b1nd_website_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B1ndWebsiteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
