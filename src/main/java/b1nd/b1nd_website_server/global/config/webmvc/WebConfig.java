7b80cbb4c972c5f50f358511e432c69992938cba
ftery0 <lerb5253@dgsw.hs.kr>
2025-06-24 12:20:58 +0900

    feat :: 업로드 file

--- src/main/java/b1nd/b1nd_website_server/global/config/webmvc/WebConfig.java
+++ src/main/java/b1nd/b1nd_website_server/global/config/webmvc/WebConfig.java
@@ -5,7 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,5 +28,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor);
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path uploadPath = Paths.get(System.getProperty("user.dir"), "uploads");
+        String uploadPathUri = uploadPath.toUri().toString();
+
+        registry.addResourceHandler("/uploads/**")  // 요청 경로
+                .addResourceLocations(uploadPathUri); // 실제 폴더 경로
     }
 }
